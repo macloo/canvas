47aa6179f38cc9a5e58074165c702944d5719f2b
Mindy McAdams <mmcadams@gmail.com>
2013-04-30 18:15:04 -0400

    Add index link to all pages

--- scripts/canvas9.js
+++ scripts/canvas9.js
@@ -8,12 +8,14 @@ if (canvas.getContext) {
 	
 	var x = 20;   // set initial position from left
 	var y = 100;  // set initial position from top
-	var color1 = "#70CCCA";
+	var color1 = "#fff";
 	var color2 = "#7ADEDC";
+	var color3 = "#D18EA9";
 	
 	function makeText(text) {
 		ctx.fillStyle = color1;
-		ctx.font      = "Bold 64px Arial";
+		ctx.font      = "64px Sofadi One";
+		// the Google font is loaded in the HEAD of the HTML file
 		ctx.fillText( text, x, y );
 		ctx.strokeText( text, x, y ); // default will be black
 	}
@@ -22,9 +24,11 @@ if (canvas.getContext) {
 	
 	function move() {
 		if (x < 601) {
+			ctx.fillStyle = color3;
+			ctx.fillRect(0,0,600,200); 
 			ctx.fillStyle = color2;
 			ctx.fillRect(0,24,600,100); 
-			// must redraw the rectangle (above) every time 
+			// must redraw the rectangles (above) every time 
 			makeText("Hello, World!");
 			x++; // move text 1 pixel to the right
 		} else {
